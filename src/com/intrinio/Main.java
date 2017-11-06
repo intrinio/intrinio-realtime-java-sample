2650b6c3130d86a69324bc5b4da2c1f51cfc8ffa
Alex Solo <alex.n.solo@gmail.com>
2017-11-06 16:16:06 -0700

    Example

---
+++ src/com/intrinio/Main.java
@@ -0,0 +1,27 @@
+package com.intrinio;
+
+import com.intrinio.realtime.Quote;
+import com.intrinio.realtime.QuoteHandler;
+import com.intrinio.realtime.RealTimeClient;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String username = "YOUR_INTRINIO_USERNAME";
+        String password = "YOUR_INTRINIO_PASSWORD";
+
+        try (RealTimeClient client = new RealTimeClient(username, password, RealTimeClient.Provider.QUODD)) {
+            client.registerQuoteHandler(new QuoteHandler() {
+                @Override
+                public void onQuote(Quote quote) {
+                    System.out.println(quote.toString());
+
+                }
+            });
+
+            String[] channels = new String[]{"BAC.NB"};
+            client.join(channels);
+            client.connect();
+        }
+    }
+}
